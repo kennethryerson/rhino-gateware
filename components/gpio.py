0b65b88201cbb813df4b8b6ec106a1577a8bed6e
Kenneth Ryerson <kenneth.ryerson@gmail.com>
2013-09-19 15:56:19 -0500

    gpio: add Pattern class

--- components/gpio.py
+++ components/gpio.py
@@ -1,6 +1,7 @@
 from migen.fhdl.std import *
 from migen.genlib.cdc import MultiReg
 from migen.bank.description import *
+from migen.genlib.fsm import FSM,NextState
 
 class GPIOIn(Module, AutoCSR):
 	def __init__(self, signal):
@@ -17,3 +18,29 @@ class Blinker(Module):
 		counter = Signal(divbits)
 		self.comb += signal.eq(counter[divbits-1])
 		self.sync += counter.eq(counter + 1)
+
+class Pattern(Module,AutoCSR):
+	def __init__(self, signal, pattern, divbits=25):
+		counter = Signal(divbits)
+		states = ["P{0}".format(s) for s in range(len(pattern))]
+		self.submodules.fsm = FSM()
+
+		self.sync += counter.eq(counter + 1)
+
+		#set up state machine
+		for i in range(len(pattern)-1):
+			self.fsm.act(states[i],
+						signal.eq(pattern[i]),
+						If(counter == 0,
+							NextState(states[i+1])
+						)
+			)
+
+		#final state returns to first state
+		self.fsm.act(states[-1],
+					signal.eq(pattern[-1]),
+					If(counter == 0,
+						NextState(states[0])
+					)
+		)
+
